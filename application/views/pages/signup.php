fb681054f591d18dd844cb09c1f79792f604523a
SammyO <sammy.odenhoven@gmail.com>
2012-03-29 07:53:12 -0400

    Pages controller merge niet. Nu samengevoegd

--- application/views/pages/signup.php
+++ application/views/pages/signup.php
@@ -2,4 +2,7 @@ Page for signing up
 <br><br>
 <form action="create" method="post">
 Username: <input type="text" name="username" id="username" size="25" required/>
-Password: <input type="password" name="passwd" id="passwd" size="25"
+Password: <input type="password" name="passwd" id="passwd" size="25" required/>
+<input type="submit" name="submit" id="submit" value="Submit"/>
+<input type="reset" name="reset" id="reset" value="Reset"/>
+</form>
